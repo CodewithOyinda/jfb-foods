ddc276f3e3ee3897ae341e6cf5229e6a9fb03df8
CodewithOyinda <honeybee08186@gmail.com>
2025-08-08 15:00:47 +0100

    hero section wip

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
 import logo from "../../assets/logo.png";
+// import { bg, flexD, px, mt } from "@/styles/global";
+import {  w, h, flex, flexD } from "../../styles/global";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaWhatsapp } from "react-icons/fa";
+import { PiPhoneCall } from "react-icons/pi";
 
 const Hero = () => {
   return (
     <div>
-      <img src={logo} className="" />
+      <div className={`${w(64)} ${h(70)}`}>
+        <img src={logo}  />
+      </div>
       
+      <div className={`${flexD("row")}`}>
+        <p>Home</p>
+        <p>Product</p>
+        <p>Contact Us</p>
+        <p>Testimony</p>
+      </div>
+      <div className={`${flexD("row")}`}>
+        <AiOutlineShoppingCart />
+        <FaWhatsapp />
+        <PiPhoneCall />
+        <p>+2348142398699</p>
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import Hero from "../components/hero/Hero";
+
 
 const Home = () => {
   return (
     <div>
+        <Hero/>
         <div>
-            
+
         </div>
       <p>HIIII</p>
     </div>
